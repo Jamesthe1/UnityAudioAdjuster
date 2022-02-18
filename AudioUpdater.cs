7151fed534ec9ec47b8ef7fdec5aef32231e3d19
James <jamesthe1sky@gmail.com>
2022-02-17 22:01:06 -0600

    Add source code

---
+++ AudioUpdater.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace AudioAdjustments {
+    public class AudioUpdater : MonoBehaviour {
+        public static GameObject Instance { get; private set; }
+        public static void Spawn () {
+            if (Instance == null)
+                Instance = new GameObject ("AudioUpdater", typeof (AudioUpdater));
+        }
+
+        // Seconds until update
+        private const float maxUpdateTime = 1f;
+        private float timeKeeper = 0f;
+        private const float adjustmentRange = 0.5f;
+        private void UpdateAudio () {
+            // Not caching this because objects are likely to be added and removed
+            AudioSource[] sources = FindObjectsOfType<AudioSource> ();
+            for (int i = 0; i < sources.Length; i++) {
+                float adjustment = Random.Range (-adjustmentRange, adjustmentRange);
+                sources[i].pitch = Mathf.Clamp (sources[i].pitch + adjustment, 0f, 50f);
+                sources[i].volume = Mathf.Clamp (sources[i].volume + adjustment, 0f, 1f);
+            }
+        }
+
+        public void Start () {
+            UpdateAudio ();
+        }
+
+        public void Update () {
+            timeKeeper += Time.deltaTime;
+            if (timeKeeper >= maxUpdateTime) {
+                UpdateAudio ();
+                timeKeeper = 0;
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using BepInEx;
+
+namespace AudioAdjustments {
+    [BepInPlugin (guid, visibleName, version)]
+    public class Plugin : BaseUnityPlugin {
+        public const string guid = "jamesthe1.audioadjustments";
+        public const string visibleName = "Audio Adjustments";
+        public const string version = "1.0.0";
+
+        private void Awake () {
+            InitHarmony ();
+            Logger.LogInfo (visibleName + " has fully loaded and is ready to go!");
+        }
+        
+        private void InitHarmony () {
+            Harmony harmony = new Harmony (guid);
+            Logger.LogInfo ("Patching with harmony...");
+            harmony.PatchAll ();
+        }
+    }
+}
